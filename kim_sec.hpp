d1b84a0f9853ad005837bd8aa2a695a7c8e07687
Eddy Kim <Eddy.M.K@outlook.com>
2022-05-09 23:48:49 -0700

    Modified top-level header files

--- kim_sec.hpp
+++ kim_sec.hpp
@@ -1,8 +1,6 @@
 #ifndef KIM_SEC
 #define KIM_SEC
 
-#include "sec_hex.hpp"
-#include "sec_bin.hpp"
-#include "sec_b64.hpp"
+#include "sec_types.hpp"
 
 #endif /* KIM_SEC */
---
+++ sec_types.hpp
@@ -0,0 +1,8 @@
+#ifndef SEC_TYPES
+#define SEC_TYPES
+
+#include "sec_hex.hpp"
+#include "sec_bin.hpp"
+#include "sec_b64.hpp"
+
+#endif /* SEC_TYPES */
