c01bca0f94c67fbfe3e0b4190d1195fdf12da6ac
Eddy Kim <Eddy.M.K@outlook.com>
2022-05-14 23:16:53 -0700

    Improved formatting and commenting

--- sec_b64.hpp
+++ sec_b64.hpp
@@ -24,27 +24,54 @@ namespace kim
         class Base64
         {
         public:
-            /* Constructors/Destructor */
+            /*** Constructors/Destructor ***/
+
+            /* Empty Constructor */
             Base64();
+
+            /* Constructor which takes in a string */
             Base64(const std::string&);
+
+            /* Destructor */
             ~Base64();
 
-            /* Public Methods */
-            std::size_t         length() const;
-            bool                empty() const;
-            void                reserve(const std::size_t);
-            Base64&             append(const std::string&);
-            Binary              to_bin() const;
-            Hex                 to_hex() const;
 
-            /* Operator */
+            /*** Public Methods ***/
+
+            /* Returns the length of the Base64 string */
+            std::size_t         length() const;
+
+            /* Returns true if the Base64 string is empty, else false */
+            bool                empty() const;
+
+            /* Reserves space for the Base64 string specified by a size_t argument */
+            void                reserve(const std::size_t);
+
+            /* Appends a string with valid Base64
+             * - Will replace any padding in the original Base64 string
+             * - If string argument contains padding, the final Base64 string must
+             *   be valid or an exception will be thrown
+             * - If string argument does not contain padding, the method will
+             *   will automatically append padding, given that str_len % 4 != 1 */
+            Base64&             append(const std::string&);
+
+            /* Returns the Binary object equivalent of the Base64 string */
+            Binary              to_Bin() const;
+
+            /* Returns the Hexadecimal object equivalent of the Base64 string */
+            Hex                 to_Hex() const;
+
+
+            /*** Operator ***/
             Base64&             operator+=(const Base64&);
 
+
         private:
-            /* Underlying Data Structure */
+            /*** Underlying Data Structure ***/
             std::string m_b64;
 
-        /* Printer */
+
+        /*** Printer ***/
         friend std::ostream& operator<<(std::ostream&, const Base64&);
         };
     }
