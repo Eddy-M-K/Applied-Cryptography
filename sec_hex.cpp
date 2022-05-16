f6fae464b4b6a825da6e690174924aa217f73845
Eddy Kim <Eddy.M.K@outlook.com>
2022-05-15 17:56:16 -0700

    Made Binary object const in to_B64()

--- sec_hex.cpp
+++ sec_hex.cpp
@@ -107,8 +107,6 @@ namespace kim
             Base64 ret{};
             ret.reserve(m_hex.length() * 2 / 3 + (m_hex.length() * 2 % 3));
 
-            Binary this_bin{this->to_Bin()};
-            std::size_t index{};
             const char base64_table[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
                                           'H', 'I', 'J', 'K', 'L', 'M', 'N',
                                           'O', 'P', 'Q', 'R', 'S', 'T', 'U',
@@ -118,6 +116,9 @@ namespace kim
                                           'q', 'r', 's', 't', 'u', 'v', 'w',
                                           'x', 'y', 'z', '0', '1', '2', '3',
                                           '4', '5', '6', '7', '8', '9', '+', '/' };
+
+            const Binary this_bin{this->to_Bin()};
+            std::size_t index{};
 
             /* Loop through every 3 bytes of the binary representation of the hexadecimal
                string but only until the set that does not need padding */
