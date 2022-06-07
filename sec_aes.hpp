1b432e2fcb19df771f56f61eabac0c9d6e416fd8
Eddy Kim <Eddy.M.K@outlook.com>
2022-06-06 23:01:43 -0700

    WIP

--- sec_aes.hpp
+++ sec_aes.hpp
@@ -4,6 +4,7 @@
 
 #include <fstream>
 
+#include "sec_xor.hpp"
 #include "sec_types.hpp"
 
 namespace kim
@@ -11,22 +12,19 @@ namespace kim
     namespace sec
     {
         template <class Container>
-        std::ofstream aes_ecb_dec(std::ifstream p_in_File, Binary p_key)
+        std::ofstream aes_ecb_dec(std::ifstream& p_in_File, const Binary& p_key)
         {
             if (p_key.length() != 16) {
                 throw std::invalid_argument("AES ECB decryption key is not 16 bytes long");
             }
 
-            std::string full_ct{};
+            std::string     full_ct{};
 
-            if (p_in_File.is_open()) {
-                std::string curr_line{};
-                while (getline(p_in_File, curr_line)) {
-                    full_ct += curr_line;
-                }
-            } else {
-                throw std::invalid_argument("Unable to open input file");
+            for (std::string curr_line{}; getline(p_in_File, curr_line);) {
+                full_ct += curr_line;
             }
+
+            Binary          full_ct_Bin{Container{full_ct}};
 
             return std::ofstream{};
         }
