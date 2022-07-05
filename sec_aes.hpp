be8378e05fccff1c980bb263e6718ba9e87e5f6e
Eddy Kim <Eddy.M.K@outlook.com>
2022-07-04 23:36:44 -0700

    WIP

--- sec_aes.hpp
+++ sec_aes.hpp
@@ -3,6 +3,7 @@
 #define SEC_AES
 
 #include <fstream>
+#include <array>
 
 #include "sec_xor.hpp"
 #include "sec_types.hpp"
@@ -11,20 +12,45 @@ namespace kim
 {
     namespace sec
     {
+        static inline void rev_sub_bytes(std::array<std::array<int, 4>, 4>& p_state_array)
+        {
+            return;
+        }
+
+        static inline void rev_shift_rows(std::array<std::array<int, 4>, 4>& p_state_array)
+        {
+            return;
+        }
+
+        static inline void rev_mix_columns(std::array<std::array<int, 4>, 4>& p_state_array)
+        {
+            return;
+        }
+
+        static inline void add_round_key(std::array<std::array<int, 4>, 4>& p_state_array)
+        {
+            return;
+        }
+
         template <class Container>
-        std::ofstream aes_ecb_dec(std::ifstream& p_in_File, const Binary& p_key)
+        std::ofstream aes_ecb_dec(std::ifstream& p_in_File, const Binary& p_key, const std::string& p_out_name)
         {
             if (p_key.length() != 16) {
                 throw std::invalid_argument("AES ECB decryption key is not 16 bytes long");
             }
 
-            std::string     full_ct{};
+            std::ofstream       ret{p_out_name};
+            std::string         full_ct{};
 
             for (std::string curr_line{}; getline(p_in_File, curr_line);) {
                 full_ct += curr_line;
             }
 
-            Binary          full_ct_Bin{Container{full_ct}};
+            Binary      full_ct_Bin{Container{full_ct}};
+
+            for (std::vector<std::byte>::size_type index{}; index < full_ct_Bin.length(); index += 16) {
+
+            }
 
             return std::ofstream{};
         }
